930110700476d4ce667f063b1aa6c18662887605
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2023-10-17 20:15:26 +0200

    uh

---
+++ sw.js
@@ -0,0 +1,27 @@
+const version = 1.4
+
+const assets = [
+    // '/',
+    // '/index.html',
+    '/script/script.js',
+    '/style/style.css',
+    '/images/monkey_192.png',
+    '/images/monkey_512.png',
+    '/manifest.json'
+]
+
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(assets);
+        })
+)});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
