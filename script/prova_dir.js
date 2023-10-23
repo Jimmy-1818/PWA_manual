c88b23fdebfb59a6828334f3e5e0408550b51dc3
Jimmy-1818 <108866410+Jimmy-1818@users.noreply.github.com>
2023-10-23 23:06:23 +0200

    d

--- script/prova_dir.js
+++ script/prova_dir.js
@@ -1,18 +1,14 @@
 async function test(){
-    alert("in test")
-    // Ottieni l'accesso alla directory specifica
-    const directoryPath = 'Scheda SD/DCIM/Camera';
-    const directory = await window.showOpenFilePicker({ startIn: directoryPath });
-''
-    // Ottieni un oggetto di tipo `FileSystemDirectoryHandle` per la directory
-    const directoryHandle = await directory.getDirectory();
+    try {
+        const directoryHandle = await window.showDirectoryPicker();
+        const entries = [];
+        
+        for await (const entry of directoryHandle.values()) {
+            entries.push(entry.name);
+        }
 
-    // Ottieni un elenco di oggetti `FileSystemHandle` per i file nella directory
-    const filesHandles = await directoryHandle.getFiles();
-
-    // Estrai i nomi dei file
-    const fileNames = filesHandles.map(fileHandle => fileHandle.name);
-
-    // Ora `fileNames` contiene un array con i nomi dei file nella directory
-    console.log(fileNames);
+        console.log(entries);
+    }catch (error) {
+        console.error(error);
+    }
 }
